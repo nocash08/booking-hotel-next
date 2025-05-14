f08ce881ccf49cfdeebc82d30721c4170a4e7d5e
Muhamad Tabina Widyatna <muhamadtabina12@gmail.com>
2025-05-15 00:17:52 +0700

    add responsive navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Raleway } from "next/font/google"
+import Navbar from "@/components/navbar/navbar"
+import "./globals.css"
 
 const raleway = Raleway({
   variable: "--font-raleway",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Online Booking Hotel",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${raleway.variable} antialiased`}
-      >
-        {children}
+      <body className={`${raleway.variable} antialiased`}>
+        <Navbar />
+        <main className="bg-gray-50 min-h-screen">{children}</main>
       </body>
     </html>
-  );
+  )
 }
