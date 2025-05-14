6bd17a47b8e76bc9e4f0a29c8a2506d6cdc2c500
Muhamad Tabina Widyatna <muhamadtabina12@gmail.com>
2025-05-15 06:41:40 +0700

    add hero section & card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Hero from "@/components/hero"
+import Main from "@/components/main"
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Home Page</h1>
+    <div>
+      <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+          <p className="py-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            eligendi!
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   )
 }
